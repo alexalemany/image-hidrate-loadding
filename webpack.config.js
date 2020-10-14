bb68f1628e995c26c5038fdeb492cea85d148952
Alex Alemany <adp.alex@gmail.com>
2020-10-14 12:31:22 +0200

    Add demo files

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,13 @@ const path = require('path')
 webpack.entry = {
     // name and input entry of your file
     // this first js is a alias js/src/js/input-file-name.js
-    'name-of-your-output-file-name': 'js/src/js/input-file-name.js',
+    'image-hydrate-loading': 'js/src/entry/index.js',
 }
 
 webpack.output = {
     filename: 'js/[name].js',
     chunkFilename: 'js/[name].js',
-    path: path.resolve(__dirname, 'path-to-your-ouput-folder'),
+    path: path.resolve(__dirname, 'demo'),
 }
 
 module.exports = webpack
