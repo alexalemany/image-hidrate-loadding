4b8ffc39878b3ae70c1307f92e10fa88ac719dc5
Alex Alemany <adp.alex@gmail.com>
2020-10-14 11:00:08 +0200

    Add source files

---
+++ src/js/image-hydrate-loading.js
@@ -0,0 +1,25 @@
+class ImageHydrateLoading {
+    constructor(selector) {
+        this.img = null
+        this.selector = (typeof selector !== 'string')
+            ? selector
+            : document.querySelector(selector)
+        console.log(this.selector)
+        this.addBufferedImage()
+    }
+
+    getAttribute(attribute) {
+        return this.selector.getAttribute(attribute)
+    }
+
+    addBufferedImage() {
+        fetch(this.getAttribute('data-src'))
+            .then((response) => response.arrayBuffer()).then((buffer) => {
+                this.selector.src = window.URL.createObjectURL(
+                    /* eslint-disable-next-line */
+                    new Blob([ new Uint8Array(buffer) ])
+                )
+                this.selector.classList.add('rendered')
+            })
+    }
+}
